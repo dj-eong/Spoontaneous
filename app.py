0dc6f0877b746a942a8803677e6f9e03903150d8
David Jeong <davidjeong221@gmail.com>
2023-11-29 18:51:45 -0500

    changed database url to not be hard-coded

--- app.py
+++ app.py
@@ -3,18 +3,20 @@ from flask import Flask, render_template, redirect, session, jsonify, flash
 from models import db, connect_db, User, SavedRecipe
 from forms import RegisterForm, LoginForm
 import requests
+import os
 
 app = Flask(__name__)
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///spoontaneous'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ckfqxttq:adoyJ81OEVNc8XvRr4jlawSmThFJP28s@bubble.db.elephantsql.com/ckfqxttq'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ckfqxttq:adoyJ81OEVNc8XvRr4jlawSmThFJP28s@bubble.db.elephantsql.com/ckfqxttq'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.environ.get('TRACK_MODIFICATIONS', False)
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
 db.create_all()
 
-app.config['SECRET_KEY'] = 'abc123'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'abc123')
 # app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 # debug = DebugToolbarExtension(app)
 
